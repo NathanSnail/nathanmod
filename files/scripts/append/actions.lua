9da5abdd00c25beaeeca75737a6f51e5734b2baa
Nahtan <47441022+NathanSnail@users.noreply.github.com>
2023-06-17 21:42:28 +1200

    Bloody Bounce

--- files/scripts/append/actions.lua
+++ files/scripts/append/actions.lua
@@ -9,11 +9,29 @@ local new_actions = {
 		spawn_level            = "0,1,2,3,4,5,6", -- BOMB
 		spawn_probability      = "1,1,1,1,0.5,0.5,0.1", -- BOMB
 		price                  = 70,
-		mana                   = 5,
-		max_uses               = 3,
+		mana                   = 35,
 		action                 = function()
 			c.fire_rate_wait = c.fire_rate_wait + 3
 			c.extra_entities = c.extra_entities .. "mods/nathanmod/files/entities/modifier/projectile_gravity.xml,"
+			draw_actions(1,true)
+		end
+	},
+	{
+		id                     = "NATHANMOD_BOUNCE_EVIL",
+		name                   = "$nathanmod_action_bounce_evil",
+		description            = "$nathanmod_actiondesc_bounce_evil",
+		sprite                 = "mods/nathanmod/files/sprites/spells/bounce_evil.png",
+		related_extra_entities = { "mods/nathanmod/files/entities/modifier/bounce_evil.xml" },
+		type                   = ACTION_TYPE_MODIFIER,
+		spawn_level            = "0,1,2,3,4,5,6", -- BOMB
+		spawn_probability      = "1,1,1,1,0.5,0.5,0.1", -- BOMB
+		price                  = 140,
+		mana                   = -25, -- still worse than add mana
+		action                 = function()
+			c.fire_rate_wait = c.fire_rate_wait - 5
+			current_reload_time = current_reload_time - 7
+			c.bounces = c.bounces + 1
+			c.extra_entities = c.extra_entities .. "mods/nathanmod/files/entities/modifier/bounce_evil.xml,"
 			draw_actions(1,true)
 		end
 	}
