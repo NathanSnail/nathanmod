3b2af3db028b5d892669e8a9c918f8c2bcb6dbca
Nahtan <47441022+NathanSnail@users.noreply.github.com>
2023-08-12 23:07:30 +1200

    boostium

---
+++ files/scripts/append/status.lua
@@ -0,0 +1,8 @@
+table.insert(status_effects, {
+	id="NATHANMOD_BOOSTIUM",
+	ui_name="$nathanmod_status_boostium",
+	ui_description="$nathanmod_statusdesc_boostium",
+	ui_icon="", -- TODO: sprite
+	effect_entity="mods/nathanmod/files/entities/status/boostium.xml",
+	is_harmful=false,
+});
--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 ModLuaFileAppend("data/scripts/gun/gun_actions.lua", "mods/nathanmod/files/scripts/append/actions.lua")
+ModLuaFileAppend("data/scripts/perks/perk_list.lua", "mods/nathanmod/files/scripts/append/perks.lua")
+ModLuaFileAppend("data/scripts/status_effects/status_list.lua", "mods/nathanmod/files/scripts/append/status.lua")
 
 local new_content = ModTextFileGetContent("mods/nathanmod/files/translations.csv")
 local content = ModTextFileGetContent("data/translations/common.csv")
 ModTextFileSetContent("data/translations/common.csv", content .. "\n" .. new_content)
+
+ModMaterialsFileAdd("mods/nathanmod/files/materials.xml")
