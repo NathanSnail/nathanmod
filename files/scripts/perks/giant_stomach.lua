a23d26c7ad463b1aadef36919b64975d95fda774
Nahtan <nathan.titirangi@gmail.com>
2023-08-12 21:41:44 +1200

    consistency fixed!

--- files/scripts/perks/giant_stomach.lua
+++ files/scripts/perks/giant_stomach.lua
@@ -1,3 +1,7 @@
+-- its not possible to use nxml to parse the ingestion effects, because we dont know what mod adds them
+giant_stomach_ghost = giant_stomach_ghost or EntityLoad("mods/nathanmod/files/entities/misc/giant_stomach_ghost.xml")
+known = known or {}
+
 local p = GetUpdatedEntityID()
 local cs = EntityGetComponent(p, "StatusEffectDataComponent") or {}
 local ic = EntityGetComponent(p, "IngestionComponent") or {}
@@ -12,7 +16,21 @@ for k, c in ipairs(cs) do
 	local d = ComponentGetValue2(c, "ingestion_effect_causes")
 	for k, v in ipairs(l) do
 		if v > 0.1 and d[k] ~= 0 then
-			EntityIngestMaterial(p, d[k], 0.71) -- 100 ~= 7s, we want 0.05s which is +0.3s/s. 100/7s*0.05s=0.71
+			if known[d[k]] == nil then
+				local old_c = EntityGetComponent(giant_stomach_ghost, "StatusEffectDataComponent")[1]
+				local old = ComponentGetValue2(old_c, "ingestion_effects")
+				EntityIngestMaterial(giant_stomach_ghost, d[k], 1)
+				for k, v in ipairs(ComponentGetValue2(old_c, "ingestion_effects")) do
+					if v - old[k] > 0.00001 then -- i am untrusting that nolla will not switch this up on me
+						known[d[k]] = v
+					end
+				end
+				-- EntityRemoveIngestionStatusEffect(giant_stomach_ghost,) we dont know the string id :sob: this means that thiings can go wrong (vomiting ghost) so we replace comp
+				EntityRemoveComponent(giant_stomach_ghost, old_c)
+				EntityAddComponent2(giant_stomach_ghost, "StatusEffectDataComponent")
+			end
+			print(known[d[k]], d[k])
+			EntityIngestMaterial(p, d[k], 0.71 * 0.075 / known[d[k]]) -- 100 ~= 7s for LC, we want 0.05s which is +0.3s/s. 100/7s*0.05s=0.71
 		end
 	end
 end
