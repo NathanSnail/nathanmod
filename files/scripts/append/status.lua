43f62513e4d9a4dfe715cdf3093bfaa1e15e1119
Nahtan <nathan.titirangi@gmail.com>
2023-08-13 21:16:18 +1200

    loads of stuff and speedrun chest

--- files/scripts/append/status.lua
+++ files/scripts/append/status.lua
@@ -1,8 +1,26 @@
 table.insert(status_effects, {
-	id="NATHANMOD_BOOSTIUM",
-	ui_name="$nathanmod_status_boostium",
-	ui_description="$nathanmod_statusdesc_boostium",
-	ui_icon="", -- TODO: sprite
-	effect_entity="mods/nathanmod/files/entities/status/boostium.xml",
-	is_harmful=false,
+	id = "NATHANMOD_BOOSTIUM",
+	ui_name = "$nathanmod_status_boostium",
+	ui_description = "$nathanmod_statusdesc_boostium",
+	ui_icon = "", -- TODO: sprite
+	effect_entity = "mods/nathanmod/files/entities/status/boostium.xml",
+	is_harmful = false,
+});
+
+table.insert(status_effects, {
+	id = "NATHANMOD_SWAPPIUM",
+	ui_name = "$nathanmod_status_swappium",
+	ui_description = "$nathanmod_statusdesc_swappium",
+	ui_icon = "", -- TODO: sprite
+	effect_entity = "mods/nathanmod/files/entities/status/swappium.xml",
+	is_harmful = false,
+});
+
+table.insert(status_effects, {
+	id = "NATHANMOD_SWAPPIUM_UNSTABLE",
+	ui_name = "$nathanmod_status_swappium_unstable",
+	ui_description = "$nathanmod_statusdesc_swappium_unstable",
+	ui_icon = "", -- TODO: sprite
+	effect_entity = "mods/nathanmod/files/entities/status/swappium_unstable.xml",
+	is_harmful = false,
 });
---
+++ files/scripts/status/swappium.lua
@@ -0,0 +1,12 @@
+local e = EntityGetRootEntity(GetUpdatedEntityID())
+local x, y = EntityGetTransform(e)
+local r = EntityGetInRadiusWithTag(x, y, 256, "enemy")
+if #r == 0 then return end
+local s = r[Random(1, #r)]
+local nx, ny = EntityGetTransform(s)
+EntitySetTransform(e, nx, ny)
+EntitySetTransform(s, x, y)
+EntityLoad("data/entities/particles/teleportation_source.xml", x, y)
+EntityLoad("data/entities/particles/teleportation_target.xml", nx, ny)
+GamePlaySound("data/audio/Desktop/misc.bank", "game_effect/teleport/tick", x, y)
+GamePlaySound("data/audio/Desktop/misc.bank", "game_effect/teleport/tick", nx, ny)
