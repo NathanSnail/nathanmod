6574cb97216bee2d0dda285bcef9cfecb7ee953d
Nahtan <47441022+NathanSnail@users.noreply.github.com>
2023-11-22 16:25:38 +1300

    smoother

--- files/scripts/spells/bubble_bubble.lua
+++ files/scripts/spells/bubble_bubble.lua
@@ -5,9 +5,10 @@ if GetUpdatedEntityID() ~= bubble_manager and EntityGetIsAlive(bubble_manager) t
 bubble_manager = GetUpdatedEntityID()
 local bubble_pool = EntityGetWithTag("bubble_bubble")
 local groups = {}
-local rad = 64
+local rad = 32
 local rad_sq = rad ^ 2
 local alpha = 0.9
+local smooth_factor = 0.4 -- maybe change this later
 -- algo overview:
 -- we have the pool which is all bubbles in a group
 -- 1 we get a bubble which is our target
@@ -56,14 +57,19 @@ for _k, group in ipairs(groups) do
 	x = x / #group
 	y = y / #group
 	local theta_part = 2 * math.pi / #group
+	table.sort(group) -- performance is already dead, this is needed for smoothing to not look bad, sort by random const >> no sort
 	for k, v in ipairs(group) do
 		local theta = theta_part * k
-		local e_rad = rad / (2 * (1 - math.cos(theta_part))) ^ 0.5 * alpha -- alpha is factor to shrink by, 1 is mathematically perfect but because velocity exists it doesn't work.
+		local e_rad = rad / (2 * (1 - math.cos(theta_part))) ^ 0.5 *
+			alpha -- alpha is factor to shrink by, 1 is mathematically perfect but because velocity exists it doesn't work.
 		if #group == 1 then
 			e_rad = 0 -- hax because 1 size group is technically stable at an infinitely large range, because it always works.
 		end
 		local xoff, yoff = e_rad * math.sin(theta), e_rad * math.cos(theta)
 		local ex, ey = x + xoff, y + yoff
+		local ox, oy = EntityGetTransform(v) -- smoother
+		local ex = ex * (1 - smooth_factor) + ox * smooth_factor
+		local ey = ey * (1 - smooth_factor) + oy * smooth_factor
 		EntitySetTransform(v, ex, ey)
 		EntityApplyTransform(v, ex, ey)
 	end
